fa1cfcffd6c550b03953678eb23e0075c1f579e9
SuGM <18888158689@163.com>
2018-09-08 21:07:59 +0800

    Initial commit

---
+++ run_provider.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+while True
+do
+    python proxy_provider.py
+    sleep 60
+done
---
+++ spiders/xici.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+1. 创建爬虫类
+2. 实现 get_proxies 方法
+3. 在 settings 中配置
+
+'''
+
+class XiciSpider(object):
+    def get_proxies(self):
+        # 编写发送请求爬取具体代理IP
+        # 把代理IP yield
+
+        pass
